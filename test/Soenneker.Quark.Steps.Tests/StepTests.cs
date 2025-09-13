7fc603a43db48e9c5f7ab209380bfe3b2e67c154
Jake Soenneker <jake@soenneker.com>
2025-09-13 16:26:46 -0500

    initial commit

---
+++ test/Soenneker.Quark.Steps.Tests/StepTests.cs
@@ -0,0 +1,18 @@
+﻿using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+namespace Soenneker.Quark.Steps.Tests;
+
+[Collection("Collection")]
+public sealed class StepTests : FixturedUnitTest
+{
+    public StepTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
